777cca1d2c7a4db08e03ab3e4034aecd36aa7a65
shanelang20 <144906694+shanelang20@users.noreply.github.com>
2023-09-20 05:38:21 -0700

    Add files via upload

---
+++ shane.py
@@ -0,0 +1,34 @@
+name = input(" name ")
+
+id = input(" id ")
+
+coursec = input(" course and section ")
+
+grade1 = float(input(" grade1 "))
+
+grade2 = float(input(" grade 2 "))
+
+grade3 = float(input(" grade 3 "))
+
+grade4 = float(input(" grade 4 "))
+
+grade =(grade1 + grade2 + grade3 + grade4) / 4
+print(" Name: ", name)
+print(" Id:", id)
+print(" course and section: ", coursec)
+print(" First Quarter Grade: ", grade1)
+print(" Second Quarter Grade: ", grade2)
+print(" Third Quarter Grade: ", grade3)
+print(" Fourth Quarter Grade: ", grade4)
+
+
+print(" The last final average is", grade)
+
+
+
+
+
+
+
+
+
